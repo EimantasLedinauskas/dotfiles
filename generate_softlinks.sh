331b7d26b57326351b64b4c029d5b790137ba299
Eimantas <eimantasl@gmail.com>
2023-11-16 17:17:05 +0200

    add tmux config

--- generate_softlinks.sh
+++ generate_softlinks.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-target="$HOME/.dotfiles/zsh/zshrc"
-link="$HOME/.zshrc"
+create_symlink() {
+    local target=$1
+    local link=$2
 
-# Check if the target file exists
-if [ ! -f "$target" ]; then
-    echo "Target file does not exist: $target"
-    exit 1
-fi
+    # Check if the target exists (file or directory)
+    if [ ! -e "$target" ]; then
+        echo "Target does not exist: $target"
+        return 1
+    fi
 
-# Remove the existing .zshrc file if it exists and is not a symbolic link
-if [ -f "$link" ] && [ ! -L "$link" ]; then
-    echo "Removing existing .zshrc file..."
-    rm "$link"
-fi
+    # Remove the existing link if it is not a symbolic link
+    if [ -e "$link" ] && [ ! -L "$link" ]; then
+        echo "Removing existing file/directory at link location: $link"
+        # Use 'rm -rf' to handle both files and directories
+        rm -rf "$link"
+    fi
 
-# Create the symbolic link
-ln -sf "$target" "$link"
-echo "Symbolic link created successfully."
+    # Create the symbolic link
+    ln -snf "$target" "$link"
+    echo "Symbolic link created for $target"
+}
+
+create_symlink "$HOME/.dotfiles/zsh/zshrc" "$HOME/.zshrc"
+create_symlink "$HOME/.dotfiles/tmux/" "$HOME/.config/tmux"
