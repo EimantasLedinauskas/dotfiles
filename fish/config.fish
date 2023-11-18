5c4f12112b5d068f0d8a5dd6406780a96f7959b6
Eimantas <eimantasl@gmail.com>
2023-11-18 15:55:10 +0200

    fzf and nnn for fish

--- fish/config.fish
+++ fish/config.fish
@@ -30,5 +30,45 @@ function fish_prompt
     set_color normal
 end
 
+# fzf
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+function fish_user_key_bindings
+	fzf_key_bindings
+end
+
+# nnn cd on quit
+function n --wraps n --description 'support nnn quit and change directory'
+    # Block nesting of nnn in subshells
+    if test -n "$NNNLVL" -a "$NNNLVL" -ge 1
+        echo "nnn is already running"
+        return
+    end
+
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "-x" from both lines below,
+    # without changing the paths.
+    if test -n "$XDG_CONFIG_HOME"
+        set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+    else
+        set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+    end
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    # The command function allows one to alias this function to `nnn` without
+    # making an infinitely recursive alias
+    command nnn -a -E -x -A -u -C $argv
+
+    if test -e $NNN_TMPFILE
+        source $NNN_TMPFILE
+        rm $NNN_TMPFILE
+    end
+end
+
 # aliases shared with bash
 source $HOME/.dotfiles/bash_zsh/aliases.sh
