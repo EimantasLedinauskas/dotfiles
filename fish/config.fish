39a536fb1bf43c566d8da8e9166273c5a369a1f0
Eimantas <eimantasl@gmail.com>
2023-11-18 15:38:19 +0200

    fish vi mode and prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,34 @@
-set -g fish_greeting
+set -g fish_greeting  # disable greeting
+fish_vi_key_bindings  # vi bindings
+function fish_mode_prompt; end  # disable default vi mode indicator
 
+# prompt
+function fish_prompt
+    set_color green
+    echo -n (whoami)
+    echo -n '@'
+    echo -n (hostname | cut -d '.' -f 1)
+    echo -n ':'
+    set_color blue
+    echo -n (prompt_pwd)
+
+    # vi mode indicator
+    set_color red --bold
+    switch $fish_bind_mode
+        case insert
+            echo -n ' > '
+        case visual
+            echo -n ' V '
+        case replace
+            echo -n ' R '
+        case replace_one
+            echo -n ' r '
+        case default
+            echo -n ' N '
+    end
+
+    set_color normal
+end
+
+# aliases shared with bash
 source $HOME/.dotfiles/bash_zsh/aliases.sh
-
