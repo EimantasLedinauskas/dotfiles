c7a55e3057c51ca2cb5830d91102a0041816c62b
Eimantas <eimantasl@gmail.com>
2023-12-24 11:23:27 +0200

    add typical cuda path

--- bash_zsh/bash_profile.sh
+++ bash_zsh/bash_profile.sh
@@ -1,3 +1,9 @@
+new_entry=/opt/cuda/bin
+case ":$PATH:" in
+  *":$new_entry:"*) :;; # already there
+  *) PATH="$new_entry:$PATH";; # or PATH="$PATH:$new_entry"
+esac
+
 if [ -n "$BASH_VERSION" ]; then
     # include .bashrc if it exists
     if [ -f "$HOME/.bashrc" ]; then
